0d9d49e55cdfd2ed75655c54fa649e01af7cb94e
Riosberg Dutra <riosbergduttra@gmail.com>
2026-03-19 16:28:09 -0300

    feat: chamando database

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
-from fastapi import FastAPI;
+from fastapi import FastAPI
+from database import get_connection
 
 app = FastAPI()
 
 @app.get("/")
 def read_root():
-    return {"Hello": "World"}
+    conn = get_connection()
+    return {"status": "ok"}
